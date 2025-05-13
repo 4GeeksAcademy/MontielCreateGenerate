ff162ee02ce5f55beb85dd72f2f74e4d6067702c
MontielCreate <luxorfer12@gmail.com>
2025-05-13 02:48:55 +0000

    generador de exc

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-import "bootstrap";
-import "./style.css";
+
 
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+import "bootstrap";
+import "./style.css";
+
+
+window.onload= function(){
+const sujetos = ["Mi perro","Mi gato","Mi loro","Mi tortuga"];
+let randomIndexSujetos = Math.floor(Math.random()*sujetos.length)
+  let sujetosRandom = sujetos[randomIndexSujetos]
+
+const acciones = ["comió","robó","perdió","destrozó"];
+let randomIndexAcciones = Math.floor(Math.random()*acciones.length)
+let accionesRandom = acciones[randomIndexAcciones]
+
+const objetos  = ["mi tarea","mis llaves","mi informe","mi teléfono"];
+let randomIndexObjetos = Math.floor(Math.random()*objetos.length)
+let objetosRandom = objetos[randomIndexObjetos]
+
+const tiempos  = ["ayer","anoche","esta mañana","hace un rato"];
+let randomIndexTiempos = Math.floor(Math.random()*tiempos.length)
+let tiemposRandom = tiempos[randomIndexTiempos]
+
+document.getElementById("excusa").innerText= sujetosRandom + " " + accionesRandom + " " + objetosRandom + " " +  tiemposRandom
+  
+  console.log(sujetosRandom, accionesRandom, objetosRandom, tiemposRandom)
+
 };
